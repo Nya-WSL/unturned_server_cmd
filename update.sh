acced91b65d7ebd3703a1c2962a463c9c58a2d6e
狐日泽 <shdocter@nya-wsl.com>
2023-06-12 22:44:32 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 steamcmd < update_cmd.txt
+ls /opt/unturned
 echo "更新完成！"
 echo "服务端保存路径：/opt/unturned"
-cd /opt/unturned && ls
 echo "如果需要在后台运行服务端请手动输入screen -S unturned创建并进入screen，然后输入'./ServerHelper.sh'启动服务器，退出screen并保持运行的快捷键为 ctrl a+d，后续进入后台指令为screen -r unturned"
